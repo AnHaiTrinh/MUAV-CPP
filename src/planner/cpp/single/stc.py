83d25b76448bf4ba52514061e92566eeab1834b4
DESKTOP-R201FAL\Thinkpad <trinhanhai1710@gmail.com>
2024-04-16 14:16:22 +0700

    Refactor STC Planner

--- src/planner/cpp/single/stc.py
+++ src/planner/cpp/single/stc.py
@@ -15,6 +15,9 @@ class SimpleSTCPlanner(SingleCPPPlanner):
         self.dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
     def plan(self) -> None:
+        def cell_to_mega_cell(cell_x: int, cell_y: int) -> tuple[int, int]:
+            return cell_x >> 1, cell_y >> 1
+
         if self.uav.r is None or self.uav.c is None:
             raise ValueError("UAV coordinates are not set")
         height, width = self.area.height, self.area.width
@@ -22,60 +25,8 @@ class SimpleSTCPlanner(SingleCPPPlanner):
         coverage_path: list[tuple[int, int]] = [start_pos]
         visited: set[tuple[int, int]] = {start_pos}
 
-        def cell_to_mega_cell(cell_x: int, cell_y: int) -> tuple[int, int]:
-            return cell_x >> 1, cell_y >> 1
-
-        def construct_mega_graph() -> Map:
-            mega_cells = []
-            for r in range(height >> 1):
-                mega_row = []
-                for c in range(width >> 1):
-                    # Assuming all four cells in a mega cell are homogeneous
-                    cell = self.area.get_cell(r << 1, c << 1)
-                    mega_row.append(Cell(cell.cell_type, cell.r, cell.c, cell.assign))
-                mega_cells.append(mega_row)
-            return Map(mega_cells)
-
-        def mst(
-            start_cell: Cell, graph: Map
-        ) -> dict[tuple[int, int], list[tuple[int, int]]]:
-            """
-            Minimum Spanning Tree algorithm
-            :return: Adjacency list of the MST
-            """
-            assignee = start_cell.assign
-            if not assignee:
-                raise ValueError("Start cell is not assigned to any UAV")
-            heap: list[tuple[int, tuple[int, int], tuple[int, int] | None]] = [
-                (0, (start_cell.r, start_cell.c), None)
-            ]  # (cost, now, parent)
-            parents = {}
-            while heap:
-                _, now, parent = heapq.heappop(heap)
-                if now in parents:
-                    continue
-                parents[now] = parent
-                now_x, now_y = now
-                for dx, dy in self.dirs:
-                    new_x, new_y = now_x + dx, now_y + dy
-                    new = (new_x, new_y)
-                    if 0 <= new_x < graph.height and 0 <= new_y < graph.width:
-                        if (
-                            new not in parents
-                            and graph.get_cell(new_x, new_y).assign == assignee
-                        ):
-                            heapq.heappush(heap, (1, new, now))
-
-            adjacency_list = defaultdict(list)
-            for child, parent in parents.items():
-                if parent is None:
-                    continue
-                adjacency_list[child].append(parent)
-                adjacency_list[parent].append(child)
-            return adjacency_list
-
-        mega_area = construct_mega_graph()
-        adj_list = mst(
+        mega_area = self._construct_mega_graph()
+        adj_list = self._mst(
             mega_area.get_cell(*cell_to_mega_cell(self.uav.r, self.uav.c)), mega_area
         )
 
@@ -139,3 +90,53 @@ class SimpleSTCPlanner(SingleCPPPlanner):
 
         coverage_trajectory = [self.area.get_cell(*pos) for pos in coverage_path]
         self.uav.update_trajectory(coverage_trajectory)
+
+    def _construct_mega_graph(self) -> Map:
+        height, width = self.area.height, self.area.width
+        mega_cells = []
+        for r in range(height >> 1):
+            mega_row = []
+            for c in range(width >> 1):
+                # Assuming all four cells in a mega cell are homogeneous
+                cell = self.area.get_cell(r << 1, c << 1)
+                mega_row.append(Cell(cell.cell_type, cell.r, cell.c, cell.assign))
+            mega_cells.append(mega_row)
+        return Map(mega_cells)
+
+    def _mst(
+            self, start_cell: Cell, graph: Map
+    ) -> dict[tuple[int, int], list[tuple[int, int]]]:
+        """
+        Minimum Spanning Tree algorithm
+        :return: Adjacency list of the MST
+        """
+        assignee = start_cell.assign
+        if not assignee:
+            raise ValueError("Start cell is not assigned to any UAV")
+        heap: list[tuple[int, tuple[int, int], tuple[int, int] | None]] = [
+            (0, (start_cell.r, start_cell.c), None)
+        ]  # (cost, now, parent)
+        parents = {}
+        while heap:
+            _, now, parent = heapq.heappop(heap)
+            if now in parents:
+                continue
+            parents[now] = parent
+            now_x, now_y = now
+            for dx, dy in self.dirs:
+                new_x, new_y = now_x + dx, now_y + dy
+                new = (new_x, new_y)
+                if 0 <= new_x < graph.height and 0 <= new_y < graph.width:
+                    if (
+                            new not in parents
+                            and graph.get_cell(new_x, new_y).assign == assignee
+                    ):
+                        heapq.heappush(heap, (1, new, now))
+
+        adjacency_list = defaultdict(list)
+        for child, parent in parents.items():
+            if parent is None:
+                continue
+            adjacency_list[child].append(parent)
+            adjacency_list[parent].append(child)
+        return adjacency_list
